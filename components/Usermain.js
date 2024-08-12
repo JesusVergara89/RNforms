a9f56ac398812598e3afef8c7a80530597f6c89c
Jesus Vergara <jesusmanuelv1989@gmail.com>
2024-08-12 15:50:14 -0600

    loggin form from scratch updates

--- components/Usermain.js
+++ components/Usermain.js
@@ -4,10 +4,10 @@ export default function Usermain({ username, setLogged, setPassword, setUsername
 
     const handleLogout = () => {
         setLogged(false);
-        setUsername(""); 
+        setUsername("");
         setPassword("");
-      };
-    
+    };
+
 
     return (
         <View style={styles.form}>
@@ -19,48 +19,20 @@ export default function Usermain({ username, setLogged, setPassword, setUsername
 
 
 const styles = StyleSheet.create({
-    container: {
-      flex: 1,
-      justifyContent: "center",
-      paddingHorizontal: 20,
-      backgroundColor: "#f5f5f5"
-    },
     form: {
-      backgroundColor: "white",
-      padding: 20,
-      borderRadius: 10,
-      shadowColor: "black",
-      shadowOffset: {
-        width: 0,
-        height: 2
-      },
-      shadowOpacity: 0.25,
-      shadowRadius: 4,
-      elevation: 5
-    },
-    label: {
-      fontSize: 16,
-      marginBottom: 5,
-      fontWeight: "bold"
-    },
-    input: {
-      height: 40,
-      borderColor: "#ddd",
-      borderWidth: 1,
-      marginBottom: 15,
-      padding: 10,
-      borderRadius: 5
-    },
-    image: {
-      width: 200,
-      height: 200,
-      alignSelf: "center",
-      marginBottom: 50
+        backgroundColor: "white",
+        padding: 20,
+        borderRadius: 10,
+        shadowColor: "black",
+        shadowOffset: {
+            width: 0,
+            height: 2
+        },
     },
     errorText: {
-      color: "red",
-      textAlign: "center",
-      fontSize: 30,
-      marginBottom: 30
+        color: "red",
+        textAlign: "center",
+        fontSize: 30,
+        marginBottom: 30
     }
-  });
+});
