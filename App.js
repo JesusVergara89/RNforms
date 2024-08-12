1adfbd04887ca6c5d141d2deb965707f1b27834e
Jesus Vergara <jesusmanuelv1989@gmail.com>
2024-08-12 14:10:10 -0600

    Switch and its props

--- App.js
+++ App.js
@@ -1,21 +1,43 @@
 import { useState } from 'react';
-import { StyleSheet, Text, View, StatusBar, SafeAreaView, TextInput } from 'react-native';
+import { StyleSheet, Text, View, StatusBar, SafeAreaView, TextInput, Switch } from 'react-native';
 
 export default function App() {
 
   const [name, setName] = useState('')
+  const [isdarkmode, setIsdarkmode] = useState(false)
 
   return (
-    <SafeAreaView style={styles.container}>
+    <SafeAreaView style={[styles.container, { backgroundColor: isdarkmode ? "black" : "white"  }]}>
       <TextInput
-      style={styles.input}
-      value={name} onChangeText={setName}
-      placeholder='example@email.com'
+        style={[styles.input, {borderColor: isdarkmode ? "white" : "black"}]}
+        value={name} onChangeText={setName}
+        placeholder='example@email.com'
       //secureTextEntry
       //keyboardType='default'
       //autoCapitalize='none'
       //autoCorrect={false}
       />
+
+      {/**Text areas */}
+      <TextInput
+        style={[styles.input, styles.multilineText, {borderColor: isdarkmode ? "white" : "black"}]}
+        placeholder='message'
+        multiline
+      />
+      {/**Text areas */}
+
+      {/**Switch */}
+      <View style={styles.switchContainer} >
+        <Text style={[styles.text, {color: isdarkmode ? "white" : "black"}]}>{isdarkmode ? 'Dark mode' : 'Light mode'}</Text>
+        <Switch
+          value={isdarkmode}
+          onValueChange={() => setIsdarkmode((previousState) => !previousState)}
+
+        />
+      </View >
+      {/**Switch */}
+
+
       <Text style={styles.text}>{name}</Text>
     </SafeAreaView>
   );
@@ -24,10 +46,9 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: 'plum',
     paddingTop: StatusBar.currentHeight
   },
-  input:{
+  input: {
     height: 40,
     margin: 12,
     padding: 10,
@@ -37,5 +58,15 @@ const styles = StyleSheet.create({
     textAlign: "center",
     fontSize: 30,
     fontWeight: "bold"
+  },
+  multilineText: {
+    minHeight: 100,
+    textAlignVertical: "top"
+  },
+  switchContainer: {
+    flexDirection: "row",
+    alignItems: "center",
+    justifyContent: "space-between",
+    paddingHorizontal: 10,
   }
 });
