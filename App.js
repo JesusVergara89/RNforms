b7c708b248234c091f681ccedf1d367eea0c01fa
Jesus Vergara <jesusmanuelv1989@gmail.com>
2024-08-12 13:39:40 -0600

    first commit and trying props of TextInput

--- App.js
+++ App.js
@@ -1,20 +1,41 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { useState } from 'react';
+import { StyleSheet, Text, View, StatusBar, SafeAreaView, TextInput } from 'react-native';
 
 export default function App() {
+
+  const [name, setName] = useState('')
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <SafeAreaView style={styles.container}>
+      <TextInput
+      style={styles.input}
+      value={name} onChangeText={setName}
+      placeholder='example@email.com'
+      //secureTextEntry
+      //keyboardType='default'
+      //autoCapitalize='none'
+      //autoCorrect={false}
+      />
+      <Text style={styles.text}>{name}</Text>
+    </SafeAreaView>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: 'plum',
+    paddingTop: StatusBar.currentHeight
   },
+  input:{
+    height: 40,
+    margin: 12,
+    padding: 10,
+    borderWidth: 1
+  },
+  text: {
+    textAlign: "center",
+    fontSize: 30,
+    fontWeight: "bold"
+  }
 });
